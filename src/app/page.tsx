b89cd5659163bd75a3b70447afe3775dd9684fc7
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-03-24 01:57:58 +0700

    minor UI updates to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,12 @@ export default function Home() {
       {/* Sticky Navbar */}
       <header className='sticky top-0 z-50 w-full bg-black/70 backdrop-blur border-b border-zinc-800 shadow-md'>
         <div className='mx-auto flex items-center justify-between px-6 py-4 max-w-7xl'>
-          <h1 className='text-2xl font-bold tracking-tight transition-all duration-300 bg-gradient-to-t from-red-700 to-orange-400 bg-clip-text text-transparent hover:drop-shadow-[0_0_10px_#f97316]'>
+          <Link
+            href='/'
+            className='text-2xl font-bold tracking-tight transition-all duration-300 bg-gradient-to-t from-red-700 to-orange-400 bg-clip-text text-transparent hover:drop-shadow-[0_0_10px_#f97316] cursor-pointer'
+          >
             YourSound<span className='text-sm align-top ml-1'>™</span>
-          </h1>
+          </Link>
           <nav className='flex items-center gap-6'>
             <Link
               href='/'
@@ -46,13 +49,13 @@ export default function Home() {
         style={{ backgroundImage: "url('/img/hero.jpg')" }}
       >
         <div className='p-6 rounded-xl max-w-3xl'>
-          <h2 className='text-4xl md:text-5xl font-extrabold mb-4'>The world of music is yours!</h2>
-          <h3 className='font-bold text-lg md:text-xl text-zinc-300 mb-8 italic'>
-            Translate any song. Understand every verse
+          <h2 className='text-4xl md:text-5xl font-extrabold mb-6'>The world of music is yours!</h2>
+          <h3 className='font-bold text-lg md:text-xl text-zinc-300 mb-9 italic'>
+            Translate any song. Understand every verse.
           </h3>
           <Button
             size='lg'
-            className='bg-gradient-to-t from-red-700 to-orange-400 text-white font-semibold hover:brightness-110'
+            className='w-48 bg-gradient-to-t from-red-700 to-orange-400 text-white font-bold hover:brightness-110 cursor-pointer'
           >
             Get Started
           </Button>
