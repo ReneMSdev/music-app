0f5dff89ee3f8e3d31b8a9c2871396f652e112f1
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-04-29 19:30:45 +0700

    Removed water pic bg from signup/login pages

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,14 +2,7 @@ import { SignUpForm } from '@/app/signup/signup-form'
 
 export default function Page() {
   return (
-    <div
-      className='flex min-h-svh w-full justify-center p-6 md:p-10'
-      style={{
-        backgroundImage: 'url(/img/water-bg.jpg)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
+    <div className='bg-stone-900 flex min-h-svh w-full justify-center p-6 md:p-10'>
       <div className='w-full max-w-sm'>
         <SignUpForm />
       </div>
